8c050525b487ac052a120399bebe3ed3bec54c29
jeskanicole <jeska_cabiles@yahoo.com>
2018-03-07 10:55:34 +0800

    db

--- prototype1/batangoble_db.php
+++ prototype1/batangoble_db.php
@@ -15,22 +15,22 @@
 		PARENT_NAME VARCHAR(100) NOT NULL,
 		PARENT_CONTACT VARCHAR(11) NOT NULL,
 		PARENT_TYPE VARCHAR(20) NOT NULL,
-		PAYMENT_TYPE VARCHAR(20) NOT NULL,
+		PAYMENT_TYPE VARCHAR(20) NOT NULL
 		/*DATE_APPLICATION DATE NOT NULL*/
 	)";
 	mysqli_query($conn, $query);
 
 	$query = "INSERT INTO STUDENT VALUES
-	('Amalia Rita Marie R. Aquino', 'F', '2016-06-22', 1, 'Thalia Ingrid Rulla', '09067228227', 'Student', 'Monthly'),
-	('Light Alexandre M. Claros', 'M', '2016-09-2016', 1, 'Johannah Marah P. Manio', '09067156445', 'Student', 'Drop in'),
-	('Ysha Nelaijah	Collado', 'F', '2013-09-28', 4, 'Mary Jane Naharilla', '09177657728', 'Alumni', 'Monthly'),
-	('Calvin Lee Custodio', 'M', '2013-08-25', 4, 'Judylyn Custodio (IM Graduate Student)', '09178795266', 'Alumni', 'Drop in'),
-	('Gibson Dwayne Dacay', 'M', '2014-05-07', 2, 'Wyndie Dacay', '09483598772', 'Admin/REPS', 'Drop in'),
-	('John Immanuel Florendo', 'M', '2016-09-08', 1, 'Ma. Rosario Florendo', '09985426982', 'Faculty', 'Monthly'),
-	('Jessica Gabatin', 'F', '2014-04-08', 3, 'Marilou L. Gabatin', '09460458446', 'Agency', 'Monthly'),
-	('Corinth Himmiwat', 'F', '2014-08-08', 3, 'Anselmo Himmiwat', '09155387700', 'Admin/REPS', 'Drop in'),
-	('Maximillian Jularbal', 'M', '2015-09-02', 1, 'Io Jularbal', '09163213466', 'Faculty', 'Monthly'),
-	";
+		('Amalia Rita Marie R. Aquino', 'F', '2016-06-22', 1, 'Thalia Ingrid Rulla', '09067228227', 'Student', 'Monthly'),
+		('Light Alexandre M. Claros', 'M', '2016-09-2016', 1, 'Johannah Marah P. Manio', '09067156445', 'Student', 'Drop in'),
+		('Ysha Nelaijah	Collado', 'F', '2013-09-28', 4, 'Mary Jane Naharilla', '09177657728', 'Alumni', 'Monthly'),
+		('Calvin Lee Custodio', 'M', '2013-08-25', 4, 'Judylyn Custodio (IM Graduate Student)', '09178795266', 'Alumni', 'Drop in'),
+		('Gibson Dwayne Dacay', 'M', '2014-05-07', 2, 'Wyndie Dacay', '09483598772', 'Admin/REPS', 'Drop in'),
+		('John Immanuel Florendo', 'M', '2016-09-08', 1, 'Ma. Rosario Florendo', '09985426982', 'Faculty', 'Monthly'),
+		('Jessica Gabatin', 'F', '2014-04-08', 3, 'Marilou L. Gabatin', '09460458446', 'Agency', 'Monthly'),
+		('Corinth Himmiwat', 'F', '2014-08-08', 3, 'Anselmo Himmiwat', '09155387700', 'Admin/REPS', 'Drop in'),
+		('Maximillian Jularbal', 'M', '2015-09-02', 1, 'Io Jularbal', '09163213466', 'Faculty', 'Monthly')
+		";
 	mysqli_query($conn, $query);
 
 
@@ -63,7 +63,7 @@
 		SA_COLLEGE VARCHAR(10) NOT NULL,
 		SA_COURSE VARCHAR(35) NOT NULL,
 		SA_YEAR VARCHAR(6) NOT NULL,
-		SA_CONTACT VARCHAR(11) NOT NULL,
+		SA_CONTACT VARCHAR(11) NOT NULL
 	)";
 	mysqli_query($conn, $query);
 
